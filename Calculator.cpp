3ed5f733ee25ef843da3b9387ed8961c825e646e
Yash Rudani <68957471+YashRudani@users.noreply.github.com>
2020-08-22 17:45:55 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int num1,int num2){
+return num1+num2;
+}
+
+int main(){
+  int num1,num2;
+ cout<<"Enter first Number:"
+ cin>>num1;
+ cout<<"Enter Second Number:"
+ cin>>num2;
+ 
+ cout<<"Addition of nos is :"<<add(num1,num2);
+  return 0;
+}
